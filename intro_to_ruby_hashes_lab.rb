c1ccd75e8600f65fc884734e1d301f6bea11f9ec
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-16 15:02:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,6 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
-      :reading_railroad => {},
       :shortline_railroad => {}
     }
   }}
@@ -53,10 +52,10 @@ def monopoly_with_fourth_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :reading_railroad => {},
+        :reading_railroad => {:mortgage_value => 100},
+        :pennsylvania_railroad => {:mortgage_value => 200},
+        :b_and_o_railroad => {:mortgage_value => 400},
+        :reading_railroad => {:mortgage_value => 800},
         :shortline_railroad => {}
       }
     }}
